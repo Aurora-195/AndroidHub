3507a95c0cd4ce14cf579471a3644ceafd8112f9
mik-fedosenko <mik-fedosenko@yandex.ru>
2024-02-25 00:23:45 -0800

    updated the App - login works

---
+++ app/src/main/java/com/example/aurorahub/LoginActivity.java
@@ -0,0 +1,151 @@
+package com.example.aurorahub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText usernameInput = findViewById(R.id.username_input);
+        EditText passwordInput = findViewById(R.id.password_input);
+        Button loginBtn = findViewById(R.id.login_btn);
+
+
+        CookieManager cookieManager = new CookieManager();
+        CookieHandler.setDefault(cookieManager);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameInput.getText().toString();
+                String password = passwordInput.getText().toString();
+
+                if (!username.isEmpty() && !password.isEmpty()){
+                    attemptToLogin(username, password);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Username or Password cannot be empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    public void attemptToLogin(String username, String password){
+        new LoginTask().execute("https://auroratime.org/users/login", username, password);
+    }
+
+    private class LoginTask extends AsyncTask<String, Void, String> {
+        // AsyncTask implementation as previously described
+        @Override
+        protected String doInBackground(String... params) {
+            String urlString = params[0]; // URL to call
+            String resultToDisplay = "";
+            InputStream in = null;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                URL url = new URL(urlString);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                urlConnection.setRequestProperty("Accept", "application/json");
+                urlConnection.setDoOutput(true);
+                urlConnection.setDoInput(true);
+
+                JSONObject jsonParam = new JSONObject();
+                jsonParam.put("login", params[1]);
+                jsonParam.put("password", params[2]);
+
+                OutputStreamWriter writer = new OutputStreamWriter(urlConnection.getOutputStream());
+                writer.write(jsonParam.toString());
+                writer.flush();
+                writer.close();
+
+                int responseCode = urlConnection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK || responseCode == HttpURLConnection.HTTP_CREATED) {
+                    in = new BufferedInputStream(urlConnection.getInputStream());
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line).append('\n');
+                    }
+                    resultToDisplay = sb.toString();
+                } else {
+                    resultToDisplay = "Error " + responseCode;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.e("LoginTask", "HTTP request error: " + e.getMessage(), e);
+                resultToDisplay +=  "Error during HTTP request: " + e.getMessage();
+
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+            return resultToDisplay;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                Log.d("LoginResponse", "Response received: " + result);
+                JSONObject jsonResponse = new JSONObject(result);
+                if (jsonResponse.has("user")) { // Check if the response contains "user" object
+                    JSONObject user = jsonResponse.getJSONObject("user");
+                    String userId = user.getString("id");
+                    String userLogin = user.getString("login");
+                    //Assuming "activities" is a JSONArray or similar
+                    String activities = user.getJSONArray("activities").toString();
+
+                    // Navigate to another activity and pass user data
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.putExtra("userId", userId);
+                    intent.putExtra("userLogin", userLogin);
+                    intent.putExtra("activities", activities);
+                    startActivity(intent);
+                } else {
+                    // Show error message
+                    String message = jsonResponse.optString("message", "Login failed. Unknown error.");
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                }
+            } catch (JSONException e) {
+                Log.e("LoginTask", "Error parsing login response: " + result, e);
+                Toast.makeText(getApplicationContext(), "Login failed. Please try again.", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        private String getPostDataString(String username, String password) throws UnsupportedEncodingException {
+            return "username=" + URLEncoder.encode(username, "UTF-8") +
+                    "&password=" + URLEncoder.encode(password, "UTF-8");
+        }
+    }
+}
--- app/src/main/java/com/example/aurorahub/MainActivity.java
+++ app/src/main/java/com/example/aurorahub/MainActivity.java
@@ -1,36 +1,27 @@
 package com.example.aurorahub;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.example.aurorahub.databinding.ActivityMainBinding;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        // Get the data passed from MainActivity
+        String userId = getIntent().getStringExtra("userId");
+        String userLogin = getIntent().getStringExtra("userLogin");
+        String activities = getIntent().getStringExtra("activities");
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        TextView message = findViewById(R.id.id_message);
+        message.setText(userId);
+        // Use the retrieved data (e.g., display it or use it in API calls)
     }
 
 }
