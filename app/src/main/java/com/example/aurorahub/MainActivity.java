8abe488212e2fa3375e0dce6b2e2e82a65e84174
pastoraljordan <alonzojordan.pastoral@sjsu.edu>
2024-04-04 17:45:10 -0700

    Animation for selecting and starting activity, need to create screen for running activity

--- app/src/main/java/com/example/aurorahub/MainActivity.java
+++ app/src/main/java/com/example/aurorahub/MainActivity.java
@@ -5,18 +5,15 @@ import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
-
-import org.w3c.dom.Text;
 
 import java.util.Arrays;
 
@@ -40,17 +37,20 @@ public class MainActivity extends AppCompatActivity {
         ImageButton activity2 = findViewById(R.id.activity2);
         ImageButton activity3 = findViewById(R.id.activity3);
         ImageButton activity4 = findViewById(R.id.activity4);
+        final int[] activity = {0};
 
         activity1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(activity1.getImageTintList() == ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.gray)))
-                    activity1.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(),R.color.light_gray)));
-                else {
-                    activity1.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.gray)));
-                    activity2.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(),R.color.light_gray)));
-                    activity3.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(),R.color.light_gray)));
-                    activity4.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(),R.color.light_gray)));
+                // Lights up the corresponding activity icon while dimming the others
+                if (activity1.getImageTintList() == ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.gray))) {
+                    activity1.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.light_gray)));
+                } else {
+                        activity1.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.gray)));
+                        activity2.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(),R.color.light_gray)));
+                        activity3.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(),R.color.light_gray)));
+                        activity4.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(),R.color.light_gray)));
+                        activity[0] = 1;
                 }
             }
         });
@@ -58,13 +58,15 @@ public class MainActivity extends AppCompatActivity {
         activity2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(activity2.getImageTintList() == ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.gray)))
-                    activity2.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(),R.color.light_gray)));
-                else {
+                // Lights up the corresponding activity icon while dimming the others
+                if(activity2.getImageTintList() == ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.gray))) {
+                    activity2.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.light_gray)));
+                } else {
                     activity2.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.gray)));
                     activity1.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(),R.color.light_gray)));
                     activity3.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(),R.color.light_gray)));
                     activity4.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(),R.color.light_gray)));
+                    activity[0] = 2;
                 }
             }
         });
@@ -72,13 +74,15 @@ public class MainActivity extends AppCompatActivity {
         activity3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(activity3.getImageTintList() == ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.gray)))
-                    activity3.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(),R.color.light_gray)));
-                else {
+                // Lights up the corresponding activity icon while dimming the others
+                if(activity3.getImageTintList() == ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.gray))) {
+                    activity3.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.light_gray)));
+                } else {
                     activity3.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.gray)));
                     activity1.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(),R.color.light_gray)));
                     activity2.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(),R.color.light_gray)));
                     activity4.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(),R.color.light_gray)));
+                    activity[0] = 3;
                 }
             }
         });
@@ -86,13 +90,15 @@ public class MainActivity extends AppCompatActivity {
         activity4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(activity4.getImageTintList() == ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.gray)))
-                    activity4.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(),R.color.light_gray)));
-                else {
+                // Lights up the corresponding activity icon while dimming the others
+                if (activity4.getImageTintList() == ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.gray))) {
+                    activity4.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.light_gray)));
+            } else {
                     activity4.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.gray)));
                     activity1.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(),R.color.light_gray)));
                     activity2.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(),R.color.light_gray)));
                     activity3.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(),R.color.light_gray)));
+                    activity[0] = 4;
                 }
             }
         });
@@ -120,8 +126,56 @@ public class MainActivity extends AppCompatActivity {
                 if(!Arrays.asList(activity1.getImageTintList(), activity2.getImageTintList(), activity3.getImageTintList(), activity4.getImageTintList())
                         .contains(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.gray)))) {
                     Snackbar.make(findViewById(R.id.start_btn), "Please select an activity", Snackbar.LENGTH_SHORT).show();
+                } else {
+                    Animation fade = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+                    switch(activity[0]) {
+                        case 1:
+                            Animation a1move = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.center_a1);
+                            activity1.startAnimation(a1move);
+                            activity2.startAnimation(fade);
+                            activity3.startAnimation(fade);
+                            activity4.startAnimation(fade);
+                            activity1.setClickable(false);
+                            activity2.setClickable(false);
+                            activity3.setClickable(false);
+                            activity4.setClickable(false);
+                            break;
+                        case 2:
+                            Animation a2move = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.center_a2);
+                            activity2.startAnimation(a2move);
+                            activity1.startAnimation(fade);
+                            activity3.startAnimation(fade);
+                            activity4.startAnimation(fade);
+                            activity1.setClickable(false);
+                            activity2.setClickable(false);
+                            activity3.setClickable(false);
+                            activity4.setClickable(false);
+                            break;
+                        case 3:
+                            Animation a3move = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.center_a3);
+                            activity3.startAnimation(a3move);
+                            activity1.startAnimation(fade);
+                            activity2.startAnimation(fade);
+                            activity4.startAnimation(fade);
+                            activity1.setClickable(false);
+                            activity2.setClickable(false);
+                            activity3.setClickable(false);
+                            activity4.setClickable(false);
+                            break;
+                        case 4:
+                            Animation a4move = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.center_a4);
+                            activity4.startAnimation(a4move);
+                            activity1.startAnimation(fade);
+                            activity2.startAnimation(fade);
+                            activity3.startAnimation(fade);
+                            activity1.setClickable(false);
+                            activity2.setClickable(false);
+                            activity3.setClickable(false);
+                            activity4.setClickable(false);
+                            break;
+                    }
+                    }
                 }
-            }
         });
 
         TextView message = findViewById(R.id.id_message);
