28ba6d214e8b63f35e935b470997d50d7367d25e
mik-fedosenko <mik-fedosenko@yandex.ru>
2024-04-21 14:25:06 -0700

    Renamed App folder for publication

---
+++ app/src/main/java/com/auroratime/aurorahub/MainActivity.java
@@ -0,0 +1,75 @@
+package com.auroratime.aurorahub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_main);
+
+        // Get the data passed from MainActivity
+        SharedPreferences sharedPreferences = getApplicationContext()
+                .getSharedPreferences("AuroraData", Context.MODE_PRIVATE);
+
+        String userId = sharedPreferences.getString("userId", "none");
+        String userLogin = sharedPreferences.getString("userLogin", "none");
+        String activities = sharedPreferences.getString("userActivities", "none");
+
+        Button addLog = findViewById(R.id.add_log_btn);
+        Button settings = findViewById(R.id.settings_btn);
+        Button signOut = findViewById(R.id.sign_out_btn);
+
+        addLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), AddLogActivity.class);
+                startActivity(intent);
+            }
+        });
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Navigate to another activity and pass user data
+
+                Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //clear saved user data
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
+
+                //open login menu
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView message = findViewById(R.id.id_message);
+        message.setText(userId);
+    }
+
+}
---
+++ app/src/main/java/com/auroratime/aurorahub/TimeConverter.java
@@ -0,0 +1,30 @@
+package com.auroratime.aurorahub;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TimeConverter {
+    public static String convertToISO8601UTC(String time) {
+        // Parse the time
+        LocalTime localTime = LocalTime.parse(time);
+        LocalDate date = LocalDate.now(ZoneId.of("America/Los_Angeles"));
+        // Combine LocalDate and LocalTime to get LocalDateTime
+        LocalDateTime localDateTime = LocalDateTime.of(date, localTime);
+
+        ZoneId pacificZone = ZoneId.of("America/Los_Angeles");
+
+        // Convert LocalDateTime to ZonedDateTime in Pacific Time
+        ZonedDateTime zonedPDT = localDateTime.atZone(pacificZone);
+
+        // Convert Pacific Time to UTC
+        ZonedDateTime zonedUTC = zonedPDT.withZoneSameInstant(ZoneId.of("UTC"));
+
+        // Format to ISO 8601 format
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        return zonedUTC.format(formatter);
+    }
+}
